97b002926baa8e61b06623a0d91f02e0e18e9827
WONDERlandian-geek <93630790+WONDERlandian-geek@users.noreply.github.com>
2021-11-21 21:49:36 -0600

    Create variablesOutline.py

---
+++ variablesOutline.py
@@ -0,0 +1,81 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+#Integer
+
+a = 5
+
+#Float
+
+a = 3.45
+
+#String
+
+a = "Samantha"
+
+#Boolean
+
+a = True
+
+a = False
+
+#List
+
+a = [1,2,3,4]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+varA = 1
+
+vara = 2
+
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+b = 36
+
+
+
+b = 51
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+k = varA
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+z = [63, 36, 9]
+
+y = 36
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+n = "Wonder"
+
+c = "W"
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print(z)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+e == 96
