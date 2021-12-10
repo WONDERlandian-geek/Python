02accdf003cfc97e7aeced94fffb5ce75ad2685d
WONDERlandian-geek <93630790+WONDERlandian-geek@users.noreply.github.com>
2021-12-09 22:04:34 -0600

    Create projectone.py

---
+++ Input/projectone.py
@@ -0,0 +1,21 @@
+'''
+The Objective is to make a program that can take survey information
+from a user.
+
+@author: Wonder
+'''
+
+name = input("What is your name?")
+print(name)
+
+color = input("What is your favorite color?")
+print(color)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friend's name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
